4585cb83ebc660c4e6397ed59f1f3a7a9be87629
Philippa Bather <philippa@nomoko.world>
2023-02-13 16:47:06 +0100

    Added onClick props

--- src/components/ui/buttons/CancelButton.jsx
+++ src/components/ui/buttons/CancelButton.jsx
@@ -1,5 +1,9 @@
-const CancelButton = () => {
-  return <button type="text">Cancel</button>;
+const CancelButton = (props) => {
+  return (
+    <button className={props.btnStyles} type="text" onClick={props.onClick}>
+      Cancel
+    </button>
+  );
 };
 
 export default CancelButton;
