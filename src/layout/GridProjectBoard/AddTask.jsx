724b033d5c803286d76c058b4d66b49548586e5f
Philippa Bather <philippa@nomoko.world>
2023-02-13 16:48:22 +0100

    Updated context call; added conditional rendering

--- src/layout/GridProjectBoard/AddTask.jsx
+++ src/layout/GridProjectBoard/AddTask.jsx
@@ -1,18 +1,22 @@
 import { useTaskContext } from "../../store/task-context.js";
 import TaskButton from "../../components/UI/Buttons/TaskButton";
+import AddTaskModal from "../../components/UI/Modals/ModalOverlay";
 import styles from "./styles.module.scss";
 
 const AddTask = () => {
-  const { openTaskCreator } = useTaskContext();
+  const { isCreateTaskOpen, openTaskCreator } = useTaskContext();
 
   const onClickHandler = () => {
     openTaskCreator();
   };
 
   return (
-    <div className={styles.addTask}>
-      <TaskButton onClick={onClickHandler} />
-    </div>
+    <>
+      {isCreateTaskOpen && <AddTaskModal />}
+      <div className={styles.addTask}>
+        <TaskButton onClick={onClickHandler} />
+      </div>
+    </>
   );
 };
 
