9d757f8d671c9bf587a74d8e78d7cfc082b9ee07
Philippa Bather <philippa@nomoko.world>
2023-02-13 22:35:08 +0100

    Added form submission &  err/notification handling

--- src/components/Tasks/TaskCreatorForm.jsx
+++ src/components/Tasks/TaskCreatorForm.jsx
@@ -1,24 +1,91 @@
+import { useState } from "react";
 import CancelButton from "../UI/Buttons/CancelButton";
 import SubmitButton from "../UI/Buttons/SubmitButton";
 import { useTaskContext } from "../../store/context-api/task-context";
+import { isNotEmpty, validateDate } from "../../utils/validation";
 import styles from "./styles.module.scss";
 
-const date = new Date();
-const localeDate = date.toLocaleDateString();
-
 const TaskCreatorForm = () => {
+  const [title, setTitle] = useState("");
+  const [priority, setPriority] = useState(0);
+  const [deadline, setDeadline] = useState("");
+  const [description, setDescription] = useState("");
+  const [isValidTitle, setIsValidTitle] = useState(false);
+  const [isValidDeadline, setIsValidDeadline] = useState(false);
+  const [isTitleTouched, setIsTitleTouched] = useState(false);
+  const [isDeadlineTouched, setIsDeadlineTouched] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
   const { closeTaskCreator } = useTaskContext();
+
+  const titleChangeHandler = (e) => {
+    const title = e.target.value;
+    setTitle(title);
+    setIsValidTitle(isNotEmpty(title));
+    setIsTitleTouched(true);
+  };
+
+  // not required; defaults to 0 (low) if no change
+  const priorityChangeHandler = (e) => {
+    setPriority(parseInt(e.target.value)); // convert to num
+  };
+
+  const deadlineChangeHandler = (e) => {
+    const deadline = e.target.value;
+    setDeadline(deadline);
+    setIsDeadlineTouched(true);
+    if (validateDate(new Date(deadline))) setIsValidDeadline(true);
+  };
+
+  // not required; no validation
+  const descriptionChangeHandler = (e) => {
+    setDescription(e.target.value.trim());
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
+
+    // validate
+    if (!isValidTitle || !isValidDeadline || title === "") return;
+
+    // if valid
+    console.log(title);
+    console.log(priority);
+    console.log(deadline);
+    console.log(description);
+
     closeTaskCreator();
-    // priority value will need changing to a Number
+
+    // reset
+    // setIsValidTitle(false);
+    // setIsValidDeadline(false);
+    // setTitle("");
+    // setPriority(0);
+    // setDeadline("");
+    // setDescription("");
   };
 
   const cancelHandler = () => {
-    console.log("cancel handler");
     closeTaskCreator();
   };
+
+  const isSubmitClicked = () => {
+    setIsSubmitted(true);
+  };
+
+  const errMsgTitle = <p className={styles.errMsg}>Task must have a title.</p>;
+
+  const errMsgDeadline = (
+    <p className={styles.inputNotification}>Please select a future date.</p>
+  );
+
+  const titleInputPrompt = (
+    <p className={styles.inputNotification}>Please enter a title.</p>
+  );
+
+  const deadlineInputPrompt = (
+    <p className={styles.inputNotification}>Please select a date.</p>
+  );
 
   return (
     <>
@@ -31,19 +98,25 @@ const TaskCreatorForm = () => {
           <input
             className={styles.formInputs}
             type="text"
-            id="taskTitle"
+            id="task-title"
             name="title"
             maxLength={50}
+            value={title}
+            onChange={titleChangeHandler}
           />
+          {isTitleTouched && !isValidTitle && errMsgTitle}
+          {isSubmitted && !isValidTitle && titleInputPrompt}
+
           <label className={styles.formLabel} htmlFor="task-priority">
             Priority
           </label>
           <select
             className={styles.formInputs}
             type="text"
-            i="task-priority"
+            id="task-priority"
             name="task-priority"
-            defaultValue="low"
+            value={priority}
+            onChange={priorityChangeHandler}
           >
             <option value="0">Low</option>
             <option value="1">Medium</option>
@@ -57,10 +130,12 @@ const TaskCreatorForm = () => {
             className={styles.formInputs}
             type="date"
             id="task-deadline"
-            min={localeDate}
             name="deadline"
-            defaultValue={localeDate}
+            value={deadline}
+            onChange={deadlineChangeHandler}
           />
+          {isDeadlineTouched && !isValidDeadline && errMsgDeadline}
+          {isSubmitted && deadline === "" && deadlineInputPrompt}
           <label className={styles.formLabel} htmlFor="task-description">
             Description
           </label>
@@ -70,11 +145,17 @@ const TaskCreatorForm = () => {
             id="task-description"
             placeholder="Add optional description..."
             maxLength={200}
+            value={description}
+            onChange={descriptionChangeHandler}
           />
         </div>
         <div className={styles.formButtonContainer}>
           <CancelButton btnStyles={styles.formButton} onClick={cancelHandler} />
-          <SubmitButton btnStyles={styles.formButton} text="Create Task" />
+          <SubmitButton
+            btnStyles={styles.formButton}
+            text="Create Task"
+            onClick={isSubmitClicked}
+          />
         </div>
       </form>
     </>
